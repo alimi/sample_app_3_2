f44240ac72cea39237bb4f3d978e4d07ef1d0ca3
Ali Ibrahim <aibrahim2k2@gmail.com>
2012-02-23 22:33:04 -0500

    Finish up to 8.1.5

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      # pending
+    else
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
